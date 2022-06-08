8704b98b3f74d5678585dc82099229fa670f501f
Robertson Akpan <robertsonakpan@gmail.com>
2022-06-08 16:31:37 +0100

    Use AAA pattern

--- stringLength.js
+++ stringLength.js
@@ -4,8 +4,9 @@ const stringLength = (str) => {
         throw new Error('string too short')
     } else if(str.length > 10){
         throw new Error('string too long')
-    }
+    }else{    
   return str.length;
+    }
 }
 
 module.exports = stringLength;
--- stringLength.test.js
+++ stringLength.test.js
@@ -1,13 +1,35 @@
 const stringLength = require('./stringLength');
 
-test('return string length', () => {
-    expect(stringLength('eleora')).toBe(6);
-});
+describe('stringLength test', () => {
+    test('return string length', () => {
+        let string = 'eleora';
 
-test('is string atleast one char and not greater than 10', () => {
-    expect(() => stringLength('')).toThrow('string too short');
-})
-
-test('is string atleast one char and not greater than 10', () => {
-    expect(() => stringLength('appreciation')).toThrow('string too long');
+        const result = stringLength(string);
+        
+        expect(result).toBe(6);
+    });
+    
+    test('return string length', () => {
+        // Arrange
+        const string = 'i';
+        // Act
+        const result = stringLength(string);
+        // Assert
+        expect(result).toBe(1);
+    });
+    
+    test('is string atleast one char and not greater than 10', () => {
+        // Arrange
+        const string = '';
+            
+        // Assert        
+        expect(() => stringLength(string)).toThrow('string too short');
+    })
+    
+    test('is string atleast one char and not greater than 10', () => {
+        // Arrange
+        const string = 'appreciation';
+        //Assert
+        expect(() => stringLength(string)).toThrow('string too long');
+    })
 })
